f6331479d051195ec9d98e6475ac6700d1ff83cb
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-14 23:50:33 +0100

    Update put_decimal.c

--- put_decimal.c
+++ put_decimal.c
@@ -1,40 +1,39 @@
-nclude "main.h"
+#include "main.h"
 /**
- * * put_decimal- function that returns an int for decimal valist
- * * @v: variable length argument list
- * * Return: int size
- * */
+* put_decimal- function that returns an int for decimal valist
+* @v: variable length argument list
+* Return: int size
+*/
 int put_decimal(va_list v)
 {
-		unsigned int number;
-			int i, div, size;
+	unsigned int number;
+	int i, div, size;
 
-				size = 0;
-					div = 1;
-						i = va_arg(v, int);
+	size = 0;
+	div = 1;
+	i = va_arg(v, int);
 
-							if (i < 0)
-									{
-												number = i * -1;
-														size += _putchar('-');
-															}
-								else
-										{
-													number = i;
-														}
+	if (i < 0)
+	{
+		number = i * -1;
+		size += _putchar('-');
+	}
+	else
+	{
+		number = i;
+	}
 
-									for (; number / div > 9;)
-											{
-														div *= 10;
-															}
+	for (; number / div > 9;)
+	{
+		div *= 10;
+	}
 
-										for (; div != 0;)
-												{
-															size += _putchar('0' + (number / div));
-																	number %= div;
-																			div /= 10;
-																				}
+	for (; div != 0;)
+	{
+		size += _putchar('0' + (number / div));
+		number %= div;
+		div /= 10;
+	}
 
-											return (size);
+	return (size);
 }
-
