ae77aa84e873258f6ba73471e84911bda24c0e33
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-14 23:48:13 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-fndef PRINTFILE_H
+#ifndef PRINTFILE_H
 #define PRINTFILE_H
 #include <stdarg.h>
 #include <stdio.h>
@@ -6,14 +6,14 @@ fndef PRINTFILE_H
 #include <string.h>
 
 /**
- *  * struct format - struct
- *   * @options: Pointer to char
- *    * @func: Pointer to function to call
- *     */
+ * struct format - struct
+ * @options: Pointer to char
+ * @func: Pointer to function to call
+ */
 typedef struct format
 {
-		char *spec;
-			int (*func)();
+	char *spec;
+	int (*func)();
 } format_t;
 int _printf(const char *format, ...);
 int formatter(const char *format, format_t options[], va_list v);
@@ -23,4 +23,3 @@ int _putchar(char c);
 int put_decimal(va_list v);
 int put_percent(__attribute__((unused))va_list v);
 #endif
-
