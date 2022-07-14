e33775b6e17295c0330c0ef65174e08adf78d3bd
Oktumas <gereddumas@gmail.com>
2022-07-14 15:52:54 -0700

    My collab project

---
+++ put_string.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+
+/**
+ * * put_string - specificer s
+ * * @v: list of arguments
+ * * Return: total characters printed
+ * */
+int put_string(va_list v)
+{
+		int i;
+			char *s;
+
+				s = va_arg(v, char*);
+
+					if (s == NULL)
+							{
+										s = "(null)";
+											}
+
+						for (i = 0; s[i] != '\0'; i++)
+								{
+											_putchar(s[i]);
+												}
+
+							return (i);
+}
+
