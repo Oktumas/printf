3f844916ddb61f61d78ed7762cc4912c18b1a035
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-14 23:43:13 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,32 +1,31 @@
-nclude "main.h"
+#include "main.h"
 /**
- * * _printf - printf clone
- * * @format: format to be printed
- * * Return: return pointer to index
- * */
+* _printf - printf clone
+* @format: format to be printed
+* Return: return pointer to index
+*/
 int _printf(const char *format, ...)
 {
-		int count;
-			va_list v;
-				format_t options[] = {
-							{"c", put_char},
-									{"s", put_string},
-											{"%", put_percent},
-													{"d", put_decimal},
-															{"i", put_decimal},
-																	{NULL, NULL}
-								};
+	int count;
+	va_list v;
+	format_t options[] = {
+		{"c", put_char},
+		{"s", put_string},
+		{"%", put_percent},
+		{"d", put_decimal},
+		{"i", put_decimal},
+		{NULL, NULL}
+	};
 
-					if (!format)
-							{
-										return (-1);
-											}
+	if (!format)
+	{
+		return (-1);
+	}
 
-						va_start(v, format);
-							count = formatter(format, options, v);
-								va_end(v);
+	va_start(v, format);
+	count = formatter(format, options, v);
+	va_end(v);
 
 
-									return (count);
+	return (count);
 }
-
