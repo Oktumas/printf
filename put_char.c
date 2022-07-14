f23675bc5fa0f3d7b4c8d927d24c4c41fc34a9ef
Oktumas <gereddumas@gmail.com>
2022-07-14 15:49:00 -0700

    My collab project

---
+++ put_char.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+
+/**
+ * * put_char - specificer s
+ * * @v: list of arguments
+ * * Return: total characters
+ * */
+int put_char(va_list v)
+{
+		_putchar(va_arg(v, int));
+			return (1);
+}
+
