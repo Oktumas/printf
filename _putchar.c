8ed088186d115dad2a40a08ad8b25173a9d7a55c
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-14 23:44:24 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,15 +1,14 @@
-nclude <unistd.h>
+#include <unistd.h>
 
 /**
- *  *  _putchar - Takes in char c and outputs to stdout
- *   *  @c: Character c to output to stdout
- *    *
- *     *  Return: Always 0 (Success)
- *      *
- *       */
+ *  _putchar - Takes in char c and outputs to stdout
+ *  @c: Character c to output to stdout
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
 
 int _putchar(char c)
 {
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
-
